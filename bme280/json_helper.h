a68ef44da91270e50b5f2328aa0913a59826133a
Andreas Schroeder <andreas@a-netz.de>
2017-08-28 17:01:38 +0800

    Add comments.

--- bme280/json_helper.h
+++ bme280/json_helper.h
@@ -1,15 +1,24 @@
 #ifndef JSON_HELPER_H
 #define JSON_HELPER_H
 
+/**
+ * Start output of a dictionary.
+ */
 #define JSON_DICT_START                         \
     Serial.print(F("{"))
 
+/**
+ * Add a float value to a dictionary.
+ */
 #define JSON_DICT_FLOAT(NAME,VAL)               \
     Serial.print(F("\""));                      \
     Serial.print(NAME);                         \
     Serial.print(F("\":"));                     \
     Serial.print(VAL)
 
+/**
+ * Add a string to a dictionary.
+ */
 #define JSON_DICT_STR(NAME,VAL)                 \
     Serial.print(F("\""));                      \
     Serial.print(NAME);                         \
@@ -19,9 +28,15 @@
     Serial.print(VAL);                          \
     Serial.print(F("\""))
 
+/**
+ * End a dictionary.
+ */
 #define JSON_DICT_END \
     Serial.print(F("}\n"))
 
+/**
+ * Add a separator (comma) between dictionary items.
+ */
 #define JSON_SEP \
     Serial.print(F(","))
 
